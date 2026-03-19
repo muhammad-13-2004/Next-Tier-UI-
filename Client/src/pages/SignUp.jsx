9964532440f26479e7fe3798c99f1821b96040f0
gptone-me <sanabano.gptone@gmail.com>
2026-03-19 13:23:09 +0500

    Added sign up image

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import AuthForm from '../components/AuthForm'
 import Logo from '../assets/LogoApp.png'
-import Formpic from '../assets/Formpic.png'
+import Formpic from '../assets/signup.png'
 
 const SignUp = () => {
   return (
-    <section className='bg-white w-screen h-screen flex overflow-hidden'>
+    <section className='bg-white w-screen h-screen flex justify-between overflow-hidden'>
 
   {/* LEFT SIDE */}
-  <div className='flex-1 flex flex-col px-16 py-10 '>
+  <div className='w-1/2 flex flex-col px-16 py-10 '>
 
     <img src={Logo} alt="logo" className='w-[120px] mb-10' />
 
@@ -19,11 +19,11 @@ const SignUp = () => {
   </div>
 
   {/* RIGHT SIDE */}
-  <div className='flex-1 h-screen'>
+  <div className='w-auto h-screen p-2'>
     <img
       src={Formpic}
       alt="form visual"
-      className='w-full h-full object-cover object-center'
+      className='w-auto h-full object-cover object-center'
     />
   </div>
 
