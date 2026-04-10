6f1abbffe854214bc20677909985d173a07ee49d
muhammad-13-2004 <muhammadabdullah13112004@gmail.com>
2026-04-10 14:25:55 +0500

    Your commit message here

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Main from './pages/Main';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
+import Main from './pages/Main'
+import SignUp from './pages/SignUp'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
